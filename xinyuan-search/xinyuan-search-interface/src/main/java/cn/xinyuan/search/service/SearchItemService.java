6f0be05ade55d36fdfa8d6b95c05d7915392e547
liweifa1994 <2414270508@qq.com>
2017-08-24 16:33:56 +0800

    solr 整合

--- xinyuan-search/xinyuan-search-interface/src/main/java/cn/xinyuan/search/service/SearchItemService.java
+++ xinyuan-search/xinyuan-search-interface/src/main/java/cn/xinyuan/search/service/SearchItemService.java
@@ -18,7 +18,7 @@ public interface SearchItemService {
      * 从solr中获取数据
      * @param keyWorkds 查询域的值
      * @param start 从第几条开始显示数据
-     * @param rows 没页显示几条数据
+     * @param rows 每页显示几条数据
      * @return
      */
     SearchResult getItemList(String keyWorkds,int start,int rows);
